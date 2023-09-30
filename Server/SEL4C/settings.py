082da6d16d5b253ad6311c6ffa3040b8d3691a02
FranciscoSanchezz <117677307+FranciscoSanchezz@users.noreply.github.com>
2023-09-29 23:01:03 -0600

    Este cambio es solo de los settings

--- Server/SEL4C/settings.py
+++ Server/SEL4C/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-at@_sb+50xxg-7ior8q)7(a6hpueuoptw2gflw#9o&w$1e944f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
@@ -126,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
