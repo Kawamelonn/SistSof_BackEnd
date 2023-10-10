620ebea3846f154d5eae53920aca24215a7a7c9d
FranciscoSanchezz <117677307+FranciscoSanchezz@users.noreply.github.com>
2023-10-10 14:40:47 -0600

    Grafica

--- Server/SEL4C/app1/static/assets/js/dashboard.js
+++ Server/SEL4C/app1/static/assets/js/dashboard.js
@@ -4,106 +4,105 @@ $(function () {
   // =====================================
   // Profit
   // =====================================
-  var chart = {
-    series: [
-      { name: "Earnings this month:", data: [355, 390, 300, 350, 390, 180, 355, 390] },
-      { name: "Expense this month:", data: [280, 250, 325, 215, 250, 310, 280, 250] },
-    ],
-
-    chart: {
-      type: "bar",
-      height: 345,
-      offsetX: -15,
-      toolbar: { show: true },
-      foreColor: "#adb0bb",
-      fontFamily: 'inherit',
-      sparkline: { enabled: false },
-    },
-
-
-    colors: ["#5D87FF", "#49BEFF"],
-
-
-    plotOptions: {
-      bar: {
-        horizontal: false,
-        columnWidth: "35%",
-        borderRadius: [6],
-        borderRadiusApplication: 'end',
-        borderRadiusWhenStacked: 'all'
-      },
-    },
-    markers: { size: 0 },
-
-    dataLabels: {
-      enabled: false,
-    },
-
-
-    legend: {
-      show: false,
-    },
-
-
-    grid: {
-      borderColor: "rgba(0,0,0,0.1)",
-      strokeDashArray: 3,
-      xaxis: {
-        lines: {
-          show: false,
-        },
-      },
-    },
-
-    xaxis: {
-      type: "category",
-      categories: ["16/08", "17/08", "18/08", "19/08", "20/08", "21/08", "22/08", "23/08"],
-      labels: {
-        style: { cssClass: "grey--text lighten-2--text fill-color" },
-      },
-    },
-
-
-    yaxis: {
-      show: true,
-      min: 0,
-      max: 400,
-      tickAmount: 4,
-      labels: {
-        style: {
-          cssClass: "grey--text lighten-2--text fill-color",
-        },
-      },
-    },
-    stroke: {
-      show: true,
-      width: 3,
-      lineCap: "butt",
-      colors: ["transparent"],
-    },
-
-
-    tooltip: { theme: "light" },
-
-    responsive: [
-      {
-        breakpoint: 600,
-        options: {
-          plotOptions: {
-            bar: {
-              borderRadius: 3,
-            }
-          },
-        }
+  fetch('http://127.0.0.1:8000//SEL4C/api/subcompetencias/')
+    .then(response => {
+      if(!response.ok){
+        throw new Error('Network response was not ok');
       }
-    ]
+      return response.json();
+    })
+      .then (data => {
+        const chartData = {
+          series: [{name:"Usuarios por subcompetencia:", data:[data.autocontrol, data.liderazgo, data.conciencia_valor_social, data.innovacion_social ]}]
+        };
+        var chartOptions = {
+          series: chartData.series,
 
+          chart: {
+            type: "bar",
+            height: 345,
+            offsetX: -15,
+            toolbar: { show: true},
+            foreColor: "#adb0bb",
+            fontFamily: "inherit",
+            sparkline: { enabled: false},
+          },
+          colors: ["#5D87FF", "#49BEFF"],
 
-  };
+          plotOptions:{
+            bar: {
+              horizontal: false,
+              columnWidth: "35%",
+              borderRadius: [6],
+              borderRadiusApplication: 'end',
+              borderRadiusWhenStacked: 'all'
+            },
+          },
+          markers: { size: 0},
+          dataLabels:{
+            enabled: false,
+          },
+          legend:{
+            show: false,
+          },
 
-  var chart = new ApexCharts(document.querySelector("#chart"), chart);
-  chart.render();
+          grid:{
+            borderColor: "rgba(0,0,0,0.1)",
+            strokeDashArray: 3,
+            xaxis:{
+              lines:{
+                show: false,
+              },
+            },
+          },
+          xaxis: {
+            type: "category",
+            categories: ["Autocontrol", "Liderazgo", "Conciencia de valor social", "Innovacion Social"],
+            labels:{
+              style: {cssClass: "grey--text lighten-2--text fill-color"},
+            },
+          },
+          
+          yaxis:{
+            show: true,
+            min: 0,
+            max: 10,
+            tickAmount: 4,
+            labels:{
+              style:{
+                cssClass: "grey--text lighten-2--text fill-color",
+              },
+            },
+          },
 
+          stroke:{
+            show: true,
+            width: 3,
+            lineCap: "butt",
+            colors: ["transparent"],
+          },
+
+          tooltip:{theme: "light"},
+
+          responsive:[
+            {
+              breakpoint: 600,
+              options: {
+                plotOptions: {
+                  bar: {
+                    borderRadius: 3,
+                  }
+                },
+              }
+            }
+          ]
+        };
+
+        var chart = new ApexCharts(document.querySelector('#chart'), chartOptions);
+        chart.render()
+      })
+    .catch(error => console.error('Error al obtener los datos:', error));
+  
 
   // =====================================
   // Breakup
